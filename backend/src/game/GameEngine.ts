4f0ca72341aa96641d8f46c55d6acd950e6cda14
Benan Redzhebov <benanredzhebov@gmail.com>
2025-04-14 19:07:51 +0200

    implemented GameState

--- backend/src/game/GameEngine.ts
+++ backend/src/game/GameEngine.ts
@@ -6,8 +6,34 @@
 /*   By: benanredzhebov <benanredzhebov@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 15:39:45 by benanredzhe       #+#    #+#             */
-/*   Updated: 2025/04/12 15:40:26 by benanredzhe      ###   ########.fr       */
+/*   Updated: 2025/04/14 16:37:57 by benanredzhe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*Core game logic (positions, collisions, scoring)*/
+
+import { GameState } from './GameState'
+
+export class GameEngine {
+	private	state: GameState;
+  
+	constructor() {
+		this.state = new GameState(); // Create a fresh game state (ball, paddles, scores)
+	}
+  
+	update(dt: number): void {
+		this.state.updateBall(dt); // Move the ball based on its velocity and time passed
+		this.state.checkCollisions(); // Check if ball hits paddles or walls
+		this.state.updateScores(); // Check if someone scored a point
+	}
+  
+	handlePlayerInput(playerId: string, direction: "up" | "down") {
+		// Let a player move their paddle up or down
+		this.state.movePaddle(playerId, direction);
+	}
+	
+	getState(): GameState {
+		// Return the current game state to send to clients
+		return this.state;
+	}
+}
--- backend/src/socket/events.ts
+++ backend/src/socket/events.ts
@@ -6,8 +6,19 @@
 /*   By: benanredzhebov <benanredzhebov@student.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/12 15:43:34 by benanredzhe       #+#    #+#             */
-/*   Updated: 2025/04/12 15:43:40 by benanredzhe      ###   ########.fr       */
+/*   Updated: 2025/04/14 16:29:11 by benanredzhe      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /* All WS event names (shared between client/server) */
+
+export const WS_EVENTS = {
+	CREATE_ROOM: "create_room",
+	JOIN_ROOM: "join_room",
+	ROOM_READY: "room_ready",
+	PLAYER_MOVE: "player_move",
+	STATE_UPDATE: "state_update",
+	SCORE_UPDATE: "score_update",
+	MATCH_END: "match_end",
+	PLAYER_DISCONNECT: "player_disconnect",
+  };
