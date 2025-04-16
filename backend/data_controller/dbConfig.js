eaace7069cb69edfad8c0b8dfd5263532136180e
Marian Sacaliuc <msacaliu@c1r15s1.42wolfsburg.de>
2025-04-16 17:18:48 +0200

    conected the back with a test front end file

---
+++ backend/data_controller/dbConfig.js
@@ -0,0 +1,7 @@
+const knex = require("knex");
+
+const config = require('./knexfile');
+
+const DB = knex(config.development);
+// npx knex migrate:make tests  //creates the data folder migration
+module.exports = DB;
