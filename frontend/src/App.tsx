ea27f897a3d0c7aee0faca157a1d0d880bd2d8e3
Marian Sacaliuc <msacaliu@c1r15s1.42wolfsburg.de>
2025-04-26 16:37:50 +0200

    solving some edge cases for sign up and logIn

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route, Link } from 'react-router-dom';
 import Game from './Game';
 import Login from './Login';
 import SignUp from './signUp'; // Import the SignUp component
+import DeleteUser from "./delete";
 
 function App() {
     return (
@@ -16,6 +17,7 @@ function App() {
                 <Route path="/logIn" element={<Login />} />
                 <Route path="/game" element={<Game />} />
                 <Route path="/signUp" element={<SignUp />} /> {/* Add the SignUp route */}
+                <Route path="/delete" element={<DeleteUser />} /> {/* Add the DeleteUser route */}
             </Routes>
         </div>
     );
