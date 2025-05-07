20bb55fac0392716785929c21647848312f76d7a
Marian Sacaliuc <msacaliu@c1r15s1.42wolfsburg.de>
2025-05-07 14:22:26 +0200

    Oauth works, still need some error checking

--- frontend/src/GetGoogleAuth.ts
+++ frontend/src/GetGoogleAuth.ts
@@ -1,22 +1,24 @@
-// // import * as https from "node:https";
-// function getGoogleAuth(){
-//
-//     const rootUrl = 'https//accounts.google.com/o/oauth2/v2/auth'
-//
-//     const options = {
-//         redirect_uri: "https://127.0.0.1:3000/username-google",
-//         client_id : "532929311202-76tdduvrs9d0oied5k4ard52r7k8pq5t.apps.googleusercontent.com",
-//         acces_type: 'offline',
-//         response_type: 'code',
-//         prompt : 'consent',
-//         scope:[
-//
-//         ]
-//
-//     }
-//     const qs = new URLSearchParams(options);
-//
-//     return `${rootUrl}?${qs.toString()}`
-// }
-//
-// export default  getGoogleAuth;
+// import * as https from "node:https";
+function getGoogleAuth(){
+
+    const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth'
+
+    const options = {
+        redirect_uri: "https://127.0.0.1:3000/username-google",
+        client_id : "532929311202-76tdduvrs9d0oied5k4ard52r7k8pq5t.apps.googleusercontent.com",
+        access_type: 'offline',
+        response_type: 'code',
+        prompt : 'consent',
+        scope:[
+            'https://www.googleapis.com/auth/userinfo.profile',
+            'https://www.googleapis.com/auth/userinfo.email',
+
+        ].join(" ")
+
+    }
+    const qs = new URLSearchParams(options);
+
+    return `${rootUrl}?${qs.toString()}`
+}
+
+export default  getGoogleAuth;
