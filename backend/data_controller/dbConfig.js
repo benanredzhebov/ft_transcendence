1fe98153a25c70191b7b088a3eccb7ef02951098
benanredzhebov <benanredzhebov@gmail.com>
2025-05-09 12:43:37 +0200

    Working on the game ogic

--- backend/data_controller/dbConfig.js
+++ backend/data_controller/dbConfig.js
@@ -1,7 +1,13 @@
-const knex = require("knex");
+// const knex = require("knex");
 
-const config = require('./knexfile');
+// const config = require('./knexfile');
+
+// const DB = knex(config.development);
+// // npx knex migrate:make tests  //creates the data folder migration
+// module.exports = DB;
+
+import knex from 'knex';
+import config from './knexfile.js';
 
 const DB = knex(config.development);
-// npx knex migrate:make tests  //creates the data folder migration
-module.exports = DB;
+export default DB;
--- backend/data_controller/knexfile.js
+++ backend/data_controller/knexfile.js
@@ -1,27 +1,20 @@
-// Update with your config settings.
+// knexfile.js
 
-/**
- * @type { Object.<string, import("knex").Knex.Config> }
- */
-//-------init knex and the data  base
-// npm install knex --save
-// npm install sqlite3
-// npx knex init
-
-module.exports = {
-
+// Use ESM syntax instead of CommonJS
+const config = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data_controller/data/myData.db'
+      filename: './data_controller/data/myData.db',
     },
     migrations: {
-      directory : './data/migrations'  //where you declere the tables, what data type and so on
+      directory: './data/migrations', // where you declare the tables
     },
-    // seeds :{  // test data to initialize the data base
-    //   directory: "./data/seeds"
+    // seeds: {
+    //   directory: './data/seeds',
     // },
-    useNullAsDefault : true,
+    useNullAsDefault: true,
   },
-
 };
+
+export default config;
