a093c2619c60659a71eb5739e5b1998ef5205a0c
Benan Redzhebov <benanredzhebov@gmail.com>
2025-06-11 21:30:53 +0200

    knexfile.js migration ran

--- backend/data_controller/knexfile.js
+++ backend/data_controller/knexfile.js
@@ -1,18 +1,39 @@
 // knexfile.js
 
 // Use ESM syntax instead of CommonJS
+// const config = {
+//   development: {
+//     client: 'sqlite3',
+//     connection: {
+//       filename: './data_controller/data/myData.db', //SQLite database file  
+//     },
+//     migrations: {
+//       directory: './data/migrations', // where you declare the tables
+//     },
+//     // seeds: {
+//     //   directory: './data/seeds',
+//     // },
+//     useNullAsDefault: true,
+//   },
+// };
+
+// export default config;
+
+// knexfile.js (ESM)
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 const config = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './data_controller/data/myData.db', //SQLite database file  
+      filename: path.resolve(__dirname, 'data/myData.db'),
     },
     migrations: {
-      directory: './data/migrations', // where you declare the tables
+      directory: path.resolve(__dirname, 'data/migrations'),
     },
-    // seeds: {
-    //   directory: './data/seeds',
-    // },
     useNullAsDefault: true,
   },
 };
