d33d2979ff5f4443d6f399aa269800da348e0957
Benan Redzhebov <benanredzhebov@gmail.com>
2025-06-24 21:12:52 +0200

    readme

--- backend/data_controller/knexfile.js
+++ backend/data_controller/knexfile.js
@@ -1,24 +1,3 @@
-// knexfile.js
-
-// Use ESM syntax instead of CommonJS
-// const config = {
-//   development: {
-//     client: 'sqlite3',
-//     connection: {
-//       filename: './data_controller/data/myData.db', //SQLite database file  
-//     },
-//     migrations: {
-//       directory: './data/migrations', // where you declare the tables
-//     },
-//     // seeds: {
-//     //   directory: './data/seeds',
-//     // },
-//     useNullAsDefault: true,
-//   },
-// };
-
-// export default config;
-
 // knexfile.js (ESM)
 import path from 'path';
 import { fileURLToPath } from 'url';
