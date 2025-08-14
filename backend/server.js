fc2137c9387be0029211774cb0dc750ba4e3649f
pfalli <pierottiandrea.falli@gmail.com>
2025-08-14 11:58:57 +0200

    styling and Invite Match

--- backend/server.js
+++ backend/server.js
@@ -55,7 +55,7 @@ if (!fs.existsSync(avatarsDir)) {
 	fs.mkdirSync(avatarsDir, { recursive: true });
 }
 
-const PORT = 8443;
+const PORT = 3000;
 const HOST = '0.0.0.0'; // Bind to all network interfaces
 
 // Load SSL certificates
